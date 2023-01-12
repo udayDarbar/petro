6a134b7cc7d1ef581d5eeee46d7e21ffc9d637d9
uday <udayrathodgameing@gmail.com>
2023-01-12 17:13:25 +0530

    finasll

---
+++ lib/colores.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color? deepPurple400 = Color.fromARGB(255, 62, 125, 180);
+Color? deepPurple50 = Colors.deepPurple[50];
+Color? pink400 = Colors.pink[400];
---
+++ lib/onboarding_contents.dart
@@ -0,0 +1,22 @@
+class OnboardingContents {
+  final String title;
+
+  final String desc;
+
+  OnboardingContents({required this.title, required this.desc});
+}
+
+List<OnboardingContents> contents = [
+  OnboardingContents(
+    title: "first make an exel file .",
+    desc: " ",
+  ),
+  OnboardingContents(
+    title: " then enter  depth in colum A& pore pressure gradiant in colum B ",
+    desc: "",
+  ),
+  OnboardingContents(
+    title: " Now its done",
+    desc: "",
+  ),
+];
